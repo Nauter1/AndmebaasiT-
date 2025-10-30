4238c1818aa3ecf372c7b89b46b2a4b3d509a6b1
Nauter <Kaur.Pakaste@gmail.com>
2025-10-30 14:06:42 +0200

    Add files via upload

---
+++ SQLQuery2.sql
@@ -0,0 +1,20 @@
+Insert into Child (FirstName, LastName, DateOfBirth) values ('John','Child','05-05-1995')
+Insert into Child (FirstName, LastName, DateOfBirth) values ('Jane','Child','05-08-1996')
+Insert into Child (FirstName, LastName, DateOfBirth) values ('Bruce','Wayne','01-05-1988')
+Insert into Child (FirstName, LastName, DateOfBirth) values ('Big','Shot','05-05-1997')
+Insert into Child (FirstName, LastName, DateOfBirth) values ('Gil','Bert','05-05-2005')
+
+Insert into [Group] (Id,ChildCount,GroupName,GroupType,RoomNumber) values ('8ce0ccde-e4c8-48f9-8d39-d3365d99b941',2,'Tester',1,241)
+Insert into [Group] (Id,ChildCount,GroupName,GroupType,RoomNumber) values ('6cb41dfa-a2db-449b-9c35-647f7ed5d311',1,'Ertest',1,246)
+Insert into [Group] (Id,ChildCount,GroupName,GroupType,RoomNumber) values ('259a4f38-c550-4d77-95e9-8844da90aa76',1,'Ertser',1,232)
+Insert into [Group] (Id,ChildCount,GroupName,GroupType,RoomNumber) values ('b42c2f7e-565b-4c2d-96cb-aec81d9cc892',2,'Teerst',1,230)
+Insert into [Group] (Id,ChildCount,GroupName,GroupType,RoomNumber) values ('22d1302d-b5c2-4c0c-9335-b3ffc69de510',1,'steter',1,125)
+
+Insert into Employee (Id, FirstName, LastName) values ('8bf93647-aa15-4646-8344-334a605acb72','John','Work')
+Insert into Employee (Id, FirstName, LastName) values ('bcccec37-c78e-4644-89e7-99fe05d5a31e','Jane','Work')
+Insert into Employee (Id, FirstName, LastName) values ('ab73565b-039a-4070-91eb-a49eabb7e293','Dr.','Who')
+Insert into Employee (Id, FirstName, LastName) values ('3f8b3b20-8617-401b-857f-01761837348f','Xavier','Zyzyva')
+Insert into Employee (Id, FirstName, LastName) values ('bd17f441-ee36-4327-bccd-056d3514c7bb','Eye','Cyu')
+
+
+Select * from Child
